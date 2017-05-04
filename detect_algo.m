37362586b9e717dd88a19654045c02b4211d89c7
wywy933 <wywy993@gmail.com>
2017-05-04 15:59:13 +0800

    added peak detect method n cal dis between peaks

--- detect_algo.m
+++ detect_algo.m
@@ -1,6 +1,6 @@
 clc;close all;clear all;
 
-raw = xlsread('limitedangletest1.xlsx','','','basic');
+raw = xlsread('limitedangletest11.xlsx','','','basic');
 
 dis = raw(1,:);
 angle = raw(2,:);
@@ -19,22 +19,87 @@ angle_step = abs((angle(1) - (angle(end)))) / length(dis);
 polar(angle,dis)
 grid on;
 [A,B] = peakdetect(dis,0.005);
-hold on;
-
-for i = 1:size(A)
-polar([0 angle(A(i,2))] , [0 A(i,1)]);
-end
-
-for i = 1:size(B)
-polar([0 angle(B(i,2))] , [0 B(i,1)]);
-end
+% hold on;
+% 
+% for i = 1:size(A)
+% polar([0 angle(A(i,2))] , [0 A(i,1)]);
+% end
+% 
+% for i = 1:size(B)
+% polar([0 angle(B(i,2))] , [0 B(i,1)]);
+% end
 
 %%%%%%%%%%%%%%%%%%xyz plot%%%%%%%%%%%%%%%%%%%%%%%%%%%
-x = zeros(1,length(dis));
-y = zeros(1,length(dis));
-for i = 1:length(dis)
-    x(i) = dis(i) * cos(angle(i));
-    y(i) = dis(i) * sin(angle(i));
-end
+% x = zeros(1,length(dis));
+% y = zeros(1,length(dis));
+% for i = 1:length(dis)
+%     x(i) = dis(i) * cos(angle(i));
+%     y(i) = dis(i) * sin(angle(i));
+% end
 % figure
 % plot(x,y)
+% grid on;
+% hold on;
+% 
+% for i = 1:size(A)
+%     plot(x(A(i,2)),y(A(i,2)),'o');
+% end
+% 
+% for i = 1:size(B)
+%     plot(x(B(i,2)),y(B(i,2)),'o');
+% end
+
+
+%%%%%%%%%%%%%rotate%%%%%%%%%%%%%%%%%%%
+x = zeros(1,length(dis));
+y = zeros(1,length(dis));
+rotate_angle = -45 * pi / 180;
+for i = 1:length(dis)
+    x(i) = dis(i) * cos(angle(i) + rotate_angle);
+    y(i) = dis(i) * sin(angle(i) + rotate_angle);
+end
+
+figure
+plot(x,y)
+grid on;
+hold on;
+[AA,BB,CC] = peakdetect(y,0.005);
+for i = 1:size(AA)
+    plot(x(AA(i,2)),y(AA(i,2)),'o');
+end
+
+for i = 1:size(BB)
+    plot(x(BB(i,2)),y(BB(i,2)),'o');
+end
+
+%%%%%%%%%%%feed rotated detection back to origenal%%%%%%%%%%%%%
+figure;
+polar(angle,dis)
+grid on;
+hold on;
+% for i = 1:size(A)
+% polar([0 angle(A(i,2))] , [0 A(i,1)]);
+% end
+% 
+% for i = 1:size(B)
+% polar([0 angle(B(i,2))] , [0 B(i,1)]);
+% end
+
+for i = 1:size(AA)
+    polar(angle(AA(i,2)),dis(AA(i,2)),'o');
+end
+
+for i = 1:size(BB)
+    polar(angle(BB(i,2)),dis(BB(i,2)),'o');
+end
+
+%%%%%%%%calculate distance between each two peaks%%%%%%%%%%%%%%%%%%%%%%%
+peak_distance = zeros(1,length(CC) - 1);
+for i = 1:length(CC) - 1
+    peak_distance(i) = polar_distance(dis(CC(i)),angle(CC(i)),dis(CC(i+1)),angle(CC(i+1)));
+
+end
+
+peak_distance
+
+
--- peakdetect.m
+++ peakdetect.m
@@ -1,8 +1,9 @@
-function [ max,min ] = peakdetect( array,threshold )
+function [ max,min,i_order ] = peakdetect( array,threshold )
 %UNTITLED2 此处显示有关此函数的摘要
 %   此处显示详细说明
 max = [];
 min = [];
+i_order = [];
 
 max_value = -Inf;
 min_value = Inf;
@@ -19,12 +20,14 @@ for i = 1:length(array)
     if lookingformax
         if this < max_value - threshold
             max = [max; max_value max_i];
+            i_order = [i_order , max_i];
             min_value = this;
             lookingformax = 0;
         end
     else
         if this > min_value + threshold
             min = [min; min_value min_i];
+            i_order = [i_order , min_i];
             max_value = this;
             lookingformax = 1;
         end
